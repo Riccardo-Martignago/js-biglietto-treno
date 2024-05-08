861ca8901c5e22733ceda45307c2a5e9db6e0b0f
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-08 20:49:27 +0200

    Sistemato valore senza sconto, snellito il codice e aggiunto stampa su foglio html

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,17 @@
-const userKm = Number.parseInt (prompt('Quanti chilometri vuoi percorrere?'));
-const userAge = Number.parseInt (prompt('Quanti anni hai?'));
-const priceInt = Number(userKm) * 0.276;
-const saleUnder = Number(priceInt) * 21 / 100;
-const saleOver = Number(priceInt) * 42 / 100;
-let saleUnderPrice = Number.parseFloat (Number(priceInt) - Number(saleUnder)).toFixed(2);
-let saleOverPrice = Number.parseFloat (Number(priceInt) - Number(saleOver)).toFixed(2);
+const userKm = prompt('Quanti chilometri vuoi percorrere?');
+const userAge = prompt('Quanti anni hai?');
+let priceInt = Number.parseFloat((userKm) * 0.276).toFixed(2);
 if ((userAge) < 18){
+    let saleUnderPrice = Number.parseFloat ((priceInt) - (priceInt) * 21 / 100).toFixed(2);
     console.log (saleUnderPrice);
+    document.getElementById ("price").innerText = saleUnderPrice
 }
 else if ((userAge) > 65){
+    let saleOverPrice = Number.parseFloat ((priceInt) - (priceInt) * 42 / 100).toFixed(2);
     console.log (saleOverPrice);
+    document.getElementById ("price").innerText = saleOverPrice
 }
 else{
     console.log (priceInt);
+    document.getElementById ("price").innerText = priceInt
 } 
